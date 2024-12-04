e1589099292f7ed88579f77b414078bb8d85aef8
Гузенко Евгений Александрович <evgeniye777.ru@mail.ru>
2024-12-04 15:32:03 +0300

    'typeDate2'

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -10,8 +10,28 @@ fun main() {
 }
 
 fun typeDate() {
-    var a :Short = 67
-    val s: String = "bvdf"
-    var b: Boolean = true
-    println(b)
+    val a: Byte = -10 //+-128
+    val b: Short = 45
+    val c: Int = -250
+    val d: Long = 30000
+
+    val aU: UByte = 10U  ///Положительные
+    val bU: UShort = 45U
+    val cU: UInt = 250U
+    val dU: ULong = 30000U
+
+    //16 ричная
+    val num: Int = 0x0A1    // 161
+    println(num) // 161
+
+    val height: Double = 1.78
+    val pi: Float = 3.14F
+    val text: String = """
+                        SALT II was a series of talks between United States
+                        and Soviet negotiators from 1972 to 1979.
+                        It was a continuation of the SALT I talks.
+                    """
+    println(text)
+
+    var name: Any = "Tom" ///любой тип
 }
