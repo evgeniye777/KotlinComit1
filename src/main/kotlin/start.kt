b90a569e2d4c6bdda5d8eb9b9ddcaeeffa0242e3
Гузенко Евгений Александрович <evgeniye777.ru@mail.ru>
2024-12-04 15:39:27 +0300

    'vivod'

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -6,32 +6,10 @@ fun main() {
     println(a)
     val b: Int = 5
     println(b + c)
-    typeDate()
+    vivod()
 }
 
-fun typeDate() {
-    val a: Byte = -10 //+-128
-    val b: Short = 45
-    val c: Int = -250
-    val d: Long = 30000
-
-    val aU: UByte = 10U  ///Положительные
-    val bU: UShort = 45U
-    val cU: UInt = 250U
-    val dU: ULong = 30000U
-
-    //16 ричная
-    val num: Int = 0x0A1    // 161
-    println(num) // 161
-
-    val height: Double = 1.78
-    val pi: Float = 3.14F
-    val text: String = """
-                        SALT II was a series of talks between United States
-                        and Soviet negotiators from 1972 to 1979.
-                        It was a continuation of the SALT I talks.
-                    """
-    println(text)
-
+fun vivod() {
     var name: Any = "Tom" ///любой тип
+    println("Ваше имя: $name") //вставка переменных в строку
 }
